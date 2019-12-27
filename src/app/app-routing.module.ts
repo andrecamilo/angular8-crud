6472983966863d2df3c621982bb29ad55adbd126
andre.camilo <andrehcamilo@gmail.com>
2019-12-27 16:11:26 -0300

    alterado docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
+import { ProductsAddComponent } from './products-add/products-add.component';
+import { ProductsEditComponent } from './products-edit/products-edit.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+    data: { title: 'List of Products' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductsDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductsAddComponent,
+    data: { title: 'Add Product' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductsEditComponent,
+    data: { title: 'Edit Product' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
+import { ProductsAddComponent } from './products-add/products-add.component';
+import { ProductsEditComponent } from './products-edit/products-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductsDetailComponent,
+    ProductsAddComponent,
+    ProductsEditComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
